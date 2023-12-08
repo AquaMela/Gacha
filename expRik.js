4c0a5e1ca9883e12742174a162e030dc46f54ca6
AquaMela <c1043958@planet.kanazawa-it.ac.jp>
2023-12-08 16:24:19 +0900

    comentout

--- expRik.js
+++ expRik.js
@@ -154,7 +154,7 @@ var stmfl = [repo_site+'img/SSR.png', repo_site+'img/SR.png', repo_site+'img/R.p
     var showimg = {
         type: 'html-keyboard-response',
         stimulus: '<img id="cursor" src="' + repo_site + 'img/mahojin.png">',
-        //duration: 5000,
+        trial_duration: 5000,
         choices: jsPsych.NO_KEYS,
         on_start: function() {
            document.addEventListener('mousemove', rotateCursor);
